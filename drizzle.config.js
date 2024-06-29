6daf36ed8ffa237df53ada7cd3d6e6644239a693
Richard Adegorite <richardadegorite@gmail.com>
2024-06-29 10:13:48 +0400

    Initial Commit

---
+++ drizzle.config.js
@@ -0,0 +1,8 @@
+
+export default {
+  schema: "./utils/schema.jsx",
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: 'postgresql://Expense-Tracker_owner:4hQ0sYVTjCNI@ep-floral-grass-a5phfvr9.us-east-2.aws.neon.tech/Expense-Tracker?sslmode=require',
+  }
+};
